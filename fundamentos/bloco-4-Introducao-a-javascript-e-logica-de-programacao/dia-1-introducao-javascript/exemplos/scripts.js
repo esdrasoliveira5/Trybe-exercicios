809438192ef2643632c0a61abcc6e81e4fbc536f
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-23 18:07:01 -0300

    Exemplos Parte 4

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exemplos/scripts.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exemplos/scripts.js
@@ -1,7 +1,8 @@
-let base = 5;
-let altura = 8;
-let area = 0;
-area = base * altura;
-console.log(area);
-let perimetro = base + base + altura + altura;
-console.log (perimetro);
+let nota = 75;
+if (nota >= 80) {
+    console.log("Parabens,voce foi aprovado!!");
+} else if (nota >= 60 && nota < 80) {
+    console.log("Voce esta na nossa lista de espera!!");
+} else {
+    console.log("Voce foi reprovado!");
+}
