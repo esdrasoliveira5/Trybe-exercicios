d8582faae3261fb3b58617a2d7fe389709f172fd
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-23 20:27:31 -0300

    Exercicio 6 completo

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio6.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio6.js
@@ -1,27 +1,27 @@
-let peca = "Cavalo";
+let peca = "CAVALO";
 
-switch (peca) {
-    case "Rei":
+switch (peca.toLocaleLowerCase()) {
+    case "rei":
         console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.");
         break;
     
-    case "Rainha":
+    case "rainha":
         console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.");
         break;
 
-    case "Bispo":
+    case "bispo":
         console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.");
         break;
 
-    case "Cavalo":
+    case "cavalo":
         console.log("O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.");
         break;
 
-    case "Torre":
+    case "torre":
         console.log("A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.");
         break;
 
-    case "Peao":
+    case "peao":
         console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal.");
         break;
         
