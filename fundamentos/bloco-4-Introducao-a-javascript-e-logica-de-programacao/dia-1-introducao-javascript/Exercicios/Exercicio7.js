3db81ee5c2f2e557adc70985610ae909e3d6aaf7
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-23 20:44:50 -0300

    Exercicio 7 Completo

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio7.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio7.js
@@ -1,1 +1,18 @@
-let nota
+let nota = 110;
+if (nota >= 90 && nota < 100) {
+     nota = "A";
+} else if (nota >= 80 && nota < 100) {
+    nota = "B";
+} else if (nota >= 70 && nota < 100) {
+    nota = "C";
+} else if (nota >= 60 && nota < 100) {
+    nota = "D"
+} else if (nota >= 50 && nota < 100) {
+    nota = "E"; 
+} else if (nota < 50) {
+    nota = "F";
+} else {
+    nota = "Erro"
+}
+
+return console.log(nota);
