af555e739135ae9cab38e29e8cf15453643eec69
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-23 18:20:26 -0300

    Exemplos Parte 5 Switch/case

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exemplos/scripts.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exemplos/scripts.js
@@ -1,8 +1,17 @@
-let nota = 75;
-if (nota >= 80) {
-    console.log("Parabens,voce foi aprovado!!");
-} else if (nota >= 60 && nota < 80) {
-    console.log("Voce esta na nossa lista de espera!!");
-} else {
-    console.log("Voce foi reprovado!");
+let concurso = "lista"
+switch (concurso) {
+    case "aprovado":
+        console.log("Parabens, voce foi aprovado!");
+        break;
+    
+    case "reprovado":
+        console.log("Voce e Burro nao passou !");
+        break;
+
+    case "lista":
+        console.log("voce esta na lista de espera!");
+        break;
+
+    default:
+        console.log("nao se aplica");
 }
