b4b7c2c00cf2606240b4beb231c6a9a88ac19285
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-23 21:57:50 -0300

    Exercicio 10 Completo

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio10.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio10.js
@@ -1,7 +1,13 @@
-let custo = 10;
-let venda = 22;
-let pImposto = 20;
-let imposto = custo * pImposto / 100;
-let custoT = custo + imposto;
+let valorCusto = 7;
+let valorVenda = 22;
 
-
+if (valorCusto == 0 || valorVenda == 0){
+    console.log("Erro");
+} else {
+    let porcentagemImposto = 20;
+    let vendas = 1000;
+    let imposto = valorCusto * porcentagemImposto / 100;
+    let custoTotal = valorCusto + imposto;
+    let lucro = valorVenda * vendas - custoTotal * vendas;
+    console.log(lucro);
+}
