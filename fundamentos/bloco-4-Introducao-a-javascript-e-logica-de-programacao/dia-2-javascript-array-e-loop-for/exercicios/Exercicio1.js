558ca58257de88e3e989b01e4bd48adef4de810d
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-24 18:35:32 -0300

    Exercicio 4 Completo

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/Exercicio1.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/Exercicio1.js
@@ -23,6 +23,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //console.log(mediaA);
 
 //Exercicio 4
+//let soma = 0;
+//let mediaA = 0;
+//for (let index = 0; index <numbers.length; index +=1) {
+//    soma += numbers[index];
+//    mediaA =  soma / numbers.length;
+//}
+//if (mediaA > 20){
+//    console.log("Valor maior que 20");
+//} else {
+//    console.log("Valor menor");
+//}
 
 
 
