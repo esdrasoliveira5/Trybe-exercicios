817ca25f37a55571eecb225b6fd13529846301d8
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-24 14:14:25 -0300

    ALgumas correcoes de acordo com as sugestoes dos colegas da Trybe

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio10.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/Exercicios/Exercicio10.js
@@ -1,12 +1,12 @@
 let valorCusto = 7;
 let valorVenda = 22;
-let porcentagemImposto = 20;
+let porcentagemImposto = 20 / 100;
 let vendas = 1000;
 
 if (valorCusto == 0 || valorVenda == 0){
     console.log("Erro");
 } else {
-    let imposto = valorCusto * porcentagemImposto / 100;
+    let imposto = valorCusto * porcentagemImposto;
     let custoTotal = valorCusto + imposto;
     let lucro = valorVenda - custoTotal;
     console.log(lucro * vendas);
