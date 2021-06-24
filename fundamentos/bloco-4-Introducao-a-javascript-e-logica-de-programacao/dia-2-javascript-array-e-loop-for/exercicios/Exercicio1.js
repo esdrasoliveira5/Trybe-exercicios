78a89126bafcddf966edb47cd264a013cd869796
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-24 18:27:35 -0300

    Exercicio 3 Completo

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/Exercicio1.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/Exercicio1.js
@@ -13,8 +13,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //}
 //console.log(soma);
 
+//Exercicio 3
+//let soma = 0;
+//let mediaA = 0;
+//for (let index = 0; index <numbers.length; index +=1) {
+//    soma += numbers[index];
+//    mediaA =  soma / numbers.length;
+//}
+//console.log(mediaA);
 
-
+//Exercicio 4
 
 
 
