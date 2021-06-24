c854445a663042e7a2124bf3022a2140dce325cd
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-24 15:58:55 -0300

    Exemplos parte 3

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exemplos/Exemplospart2.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exemplos/Exemplospart2.js
@@ -1,10 +1,21 @@
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for (let index = 0; index < groceryList.length; index += 1) {
-    console.log(groceryList[index]);
+//for (let index = 0; index < groceryList.length; index += 1) {
+   // console.log(groceryList[index]);
+
+//tabuada
+//let numero = 7;
+//for (let contador = 1; contador <= 10; contador += 1) {
+  //  console.log(numero * contador);
+//}
+
+
+// montagem de menssagem com os nomes da lista
+let nomes = ["aba","jum","beta","teta","caca"];
+
+for (let index = 0; index < nomes.length; index += 1) {
+    let men = "Boa foda, " + nomes[index] + "!!!??";
+    console.log(men); 
 }
-
-
-
 
 
