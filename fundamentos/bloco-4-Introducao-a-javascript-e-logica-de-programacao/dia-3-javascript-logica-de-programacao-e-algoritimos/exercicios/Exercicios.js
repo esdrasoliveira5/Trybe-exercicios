bfbdefed2ebabc507eaa940fa9c59d76a996e67d
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-25 19:09:01 -0300

    Feito Exercicio 1

---
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicios/Exercicios.js
@@ -0,0 +1,16 @@
+
+//Exercicio 1
+//Declarar a variavel n;
+//Declarar a variavel "*";
+//Declarar a
+//
+let n = 5;
+let a = "*";
+let b = "";
+for (i = 0; i < n; i += 1) {
+    b = b + a;
+}
+console.log(b);
+for (i = 0; i < n; i += 1) {
+    console.log(b);
+}
