05eb6fda62490a9275d6da632d58440ac23b367a
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-25 20:09:05 -0300

    Exercicio 3 Completo

---
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/Exercicio3.js
@@ -0,0 +1,25 @@
+//Exercicio 3
+//Criar a variavel a com o asterisco;
+//Criar a variavel b com o espaco;
+//Criar a variavel c vazia;
+//Criar o loop 
+//
+//
+
+
+let n = 5;
+let a = "*";
+let c = "";
+let b = n;
+for(let li = 0; li < n; li += 1 ) {
+    for (let ci = 0; ci <= n; ci += 1) {
+       if (ci) 
+    }
+}
+
+
+
+
+
+
+
--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicios/Exercicios.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicios/Exercicios.js
@@ -2,15 +2,20 @@
 //Exercicio 1
 //Declarar a variavel n;
 //Declarar a variavel "*";
-//Declarar a
-//
+//Declarar a variavel "";
+//Criar um for para fazer o loop da concatenacao;
+//Criar o codigo que vai concatenar a variavel a com o asterisco e a variavel b vazia;
+// Criar um for para imprimir a variavel b coma linha dos asteriscos;
+
 let n = 5;
 let a = "*";
 let b = "";
-for (i = 0; i < n; i += 1) {
+for (let i = 0; i < n; i += 1) {
     b = b + a;
 }
-console.log(b);
-for (i = 0; i < n; i += 1) {
+for (let i = 0; i < n; i += 1) {
     console.log(b);
 }
+
+
+
