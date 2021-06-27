476af63c48767a77db2e3e73bfe41efd2352ef8e
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-27 11:27:14 -0300

    Exercicio 3 Completo

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/Exercicio3.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/Exercicio3.js
@@ -3,23 +3,35 @@
 //Criar a variavel b com o espaco;
 //Criar a variavel c vazia;
 //Criar o loop 
+//criar um segundo loop para adicionar espacos
 //
-//
+
+
+//let n = 10;
+//let a = "*";
+//let b = "";
+//for (let i = 0; i < n; i += 1) {
+//    b += a;
+//    console.log(b);
+//}
 
 
 let n = 5;
-let a = "*";
-let c = "";
-let b = n;
-for(let li = 0; li < n; li += 1 ) {
-    for (let ci = 0; ci <= n; ci += 1) {
-       if (ci) 
+let symbol = '*';
+let space = ' ';
+let imputLine = '';
+let imputPosition = n;
+for (lineIndex = 0; lineIndex <= n; lineIndex += 1){
+    for (columIndex = 0; columIndex < n; columIndex +=1){
+        if (columIndex < imputPosition){
+            imputLine = imputLine + space;
+        } else { 
+            imputLine = imputLine + symbol;
+        }
     }
-}
-
-
-
-
-
+    console.log(imputLine);
+    imputLine = '';
+    imputPosition -= 1;
+};
 
 
