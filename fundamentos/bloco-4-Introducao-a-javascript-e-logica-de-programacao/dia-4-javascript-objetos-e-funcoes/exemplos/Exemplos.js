5c4e43989c5958219f26432ea3498b2102d2891f
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-28 15:32:20 -0300

    Exemplos 5 Completo

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exemplos/Exemplos.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exemplos/Exemplos.js
@@ -2,7 +2,12 @@
 let name = 'Marta';
 let lastName = 'Silva';
 let age = 34;
-let medals = { golden: 2, silver: 3 };
+let medals = { 
+    golden: 2, 
+    silver: 3 
+};
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
 //Exercicio 1
 let player = {
     name: 'Marta',
@@ -11,12 +16,15 @@ let player = {
     medals: {
         golden: 2,
         silver:3
-    }
+    },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 } 
 //Exercicio 2   
 console.log(player.name +' ' + player.lastName + ' ' + player.age);
-
-
+//Exercicio 4 
+console.log("A Jogadora " + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes: ' + player.bestInTheWorld);
+// Exercicio 5
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.' )
 
 
 
