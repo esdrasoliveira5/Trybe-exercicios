9bb201e46488efce9de3c7ff85e8e0e7b5723218
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-28 23:57:55 -0300

    CANSEI POR HOJE

--- fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/ExerciciosParte2.js
+++ fundamentos/bloco-4-Introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/ExerciciosParte2.js
@@ -98,8 +98,25 @@ sumNumbers(3);
 
 
 
+//Exercicio 7
+// Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+// Valor de teste: 'trybe' e 'be'
+// Valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be') ;
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan') ;
+// Retorno esperado: false
 
+function verifyWordEnding(word, ending) {
+    if (word.length > ending.length && word[-1,-2,-3] === ending[-1,-2,-3]) {
+        console.log("True")
+    } else {
+        console.log(false);
 
+    }
+}
+
+verifyWordEnding('trybe', 'be');
 
 
  
