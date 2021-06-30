38ad698f0f44b5f06ea78bab67500c43605ced49
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-06-30 15:34:50 -0300

    Exemplos parte 2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exemplos/scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Louro quer biscoito!!"
+document.getElementById("paragraph").innerText = "Eu sou o Homem de Marte "
+document.getElementById("subtitle").innerText= "Tenho mais de mil anos"
