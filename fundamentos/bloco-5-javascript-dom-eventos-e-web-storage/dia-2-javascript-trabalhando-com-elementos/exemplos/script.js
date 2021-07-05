820989f3dcbade50def521a6df95970afe9567c1
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-05 18:39:11 -0300

    Exercicios bloco 5.2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exemplos/script.js
@@ -0,0 +1,15 @@
+document.querySelector("#elementoOndeVoceEsta").parentNode.style.backgroundColor = "blue";
+
+document.querySelector("#primeiroFilhoDoFilho").innerText = "O Mais Barato do Mercado!";
+
+document.querySelector("#pai").firstChild;
+
+document.createElement()
+
+
+
+
+
+
+
+
