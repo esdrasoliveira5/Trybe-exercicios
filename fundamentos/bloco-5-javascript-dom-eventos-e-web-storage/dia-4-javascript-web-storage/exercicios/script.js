834edc00ff60abc296404604185d2a7cb74dd42c
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-08 11:36:42 -0300

    localStorage fontSize, backgroundColor and fontColor created

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
@@ -5,12 +5,14 @@ window.onload = function() {
     backgroundColor.addEventListener("change", changeBackgroudColor);
     function changeBackgroudColor () {
         document.body.style.backgroundColor = backgroundColor.value;
+        localStorage.setItem("backgroundColor", backgroundColor.value);
     };
 
     let fontColor = document.getElementById("fontColor");
     fontColor.addEventListener("change", changeFontColor);
     function changeFontColor () {
         document.body.style.color = fontColor.value;
+        localStorage.setItem("fontColor", fontColor.value);
     };
 
 
@@ -19,7 +21,19 @@ window.onload = function() {
     function changeFontSize () {
        let p = document.querySelector("p");
        p.style.fontSize = fontSize.value + "px";
+       localStorage.setItem("fontSize", fontSize.value + "px");
     };
+
+    let savedBackgroundColor = localStorage.getItem("backgroundColor");
+    document.body.style.backgroundColor = savedBackgroundColor;
+
+    let savedFontColor = localStorage.getItem("fontColor");
+    document.body.style.color = savedFontColor;
+
+    let savedFontSize = localStorage.getItem("fontSize");
+    document.querySelector("p").style.fontSize = savedFontSize;
+
+
 
 
 };
