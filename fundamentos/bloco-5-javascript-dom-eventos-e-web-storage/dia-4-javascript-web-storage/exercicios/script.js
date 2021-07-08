88c93cb72cfef078e8c9b2504048caaa6c9d4530
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-08 10:54:26 -0300

    Font Color Complete

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
@@ -2,13 +2,16 @@ window.onload = function() {
     console.log("Funcionando");
     
     let backgroundColor = document.getElementById("backgroundColor");
-    
-    backgroundColor.addEventListener("change", changeBackgroudColor )
-
+    backgroundColor.addEventListener("change", changeBackgroudColor);
     function changeBackgroudColor () {
         document.body.style.backgroundColor = backgroundColor.value;
     };
 
+    let fontColor = document.getElementById("fontColor");
+    fontColor.addEventListener("change", changeFontColor);
+    function changeFontColor () {
+        document.body.style.color = fontColor.value;
+    };
 
 
 };
