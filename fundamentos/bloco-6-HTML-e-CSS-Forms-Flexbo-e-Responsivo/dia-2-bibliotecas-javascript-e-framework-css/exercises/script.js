5af34fb3d7b572a542fc94ae738889bde888fb78
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-15 22:03:48 -0300

    Exercicio Completo

--- fundamentos/bloco-6-HTML-e-CSS-Forms-Flexbo-e-Responsivo/dia-2-bibliotecas-javascript-e-framework-css/exercises/script.js
+++ fundamentos/bloco-6-HTML-e-CSS-Forms-Flexbo-e-Responsivo/dia-2-bibliotecas-javascript-e-framework-css/exercises/script.js
@@ -1,6 +1,8 @@
 const estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 const estadosCompleto = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
 const selectEstados = document.querySelector('#estados-input');
+const buttonSubmit = document.querySelector('#buttonSubmit');
+const buttonReset = document.querySelector('#resetButton');
 
 for(let i = 0; i < estados.length; i += 1) {
   const options = document.createElement('option');
@@ -8,4 +10,17 @@ for(let i = 0; i < estados.length; i += 1) {
   options.innerText = estadosCompleto[i];
   options.value = estados[i];
 }
+
+function submit(event) {
+  event.preventDefault();
+  //validation.init();
+  validation.highlight();
+}
+buttonSubmit.addEventListener('click', submit);
+
+
+function resete() {
+  document.getElementById("client").reset();
+}
+buttonReset.addEventListener('click', resete)
 
