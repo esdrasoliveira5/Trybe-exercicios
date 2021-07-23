66335fd5b6b2d48c78e4e8c3363a9bff9c84dfc3
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-23 20:45:10 -0300

    Parte 2 Completa

--- fundamentos/bloco-7-Introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicios/Parte2.js
+++ fundamentos/bloco-7-Introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicios/Parte2.js
@@ -22,6 +22,64 @@ const addTurno = (objeto, key, value) => {
   objeto[key] = value;
   return objeto;
 }
+//Exercicio 1
+//console.log(addTurno(lesson2, 'turno', 'manhã'));
 
-console.log(addTurno(lesson2, 'turno', 'manhã'));
+//Exercicio 2
+const keysObject = (object) => Object.keys(object);
 
+//console.log(keysObject(lesson3));
+
+//Exercicio 3
+const lengthObject = (object) => Object.entries(object).length;
+
+//console.log((lengthObject(lesson3)));
+
+//Exercicio 4
+const valueObject = (object) => Object.values(object);
+
+//console.log(valueObject(lesson1));
+
+//Exercicio 5
+
+const allLessons = {
+};
+
+ Object.assign(allLessons, allLessons['lesson1']= lesson1, allLessons['lesson2'] = lesson2, allLessons['lesson3'] = lesson3);
+
+//console.log(allLessons);
+
+//Exercicio 6
+
+const studentsNumbers = (object) => {
+  let soma = object.lesson1.numeroEstudantes + object.lesson2.numeroEstudantes + object.lesson3.numeroEstudantes;
+
+  return soma;
+}
+
+//console.log(studentsNumbers(allLessons));
+
+//Exercicio 7
+
+const getValuebyNumber = (object, number) => {
+  let valor = Object.values(object)[number];
+  return valor
+}
+
+//console.log(getValuebyNumber(lesson1, 1));
+
+//Exercicio 8
+
+
+const verifyPair = (object, key, value) => {
+  let keyValue = Object.entries(object);
+  let output = false;
+
+  for (let kv of keyValue) {
+    kv[0] === key && kv[1] === value ? output = true : output = false;
+  }
+  return output;
+}
+
+
+console.log(verifyPair(lesson3, 'turno', 'noite'));
