be8d6c4e4aa095d923c5a88281f5b035ddbcb025
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-28 15:50:37 -0300

    Exemplo Fixicacao

---
+++ fundamentos/bloco-8-higher-order-functions-js-es6/exemplos/exemplos.js
@@ -0,0 +1,9 @@
+const wakeUp = () => 'Acordando!!';
+
+const coffeBreak = () => 'Bora tomar cafe!!';
+
+const sleepTime = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func());
+
+doingThings(coffeBreak);
