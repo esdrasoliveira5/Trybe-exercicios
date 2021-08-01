2b51748aaec18fcbace33583fc38dac36bd92d01
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-08-01 20:11:55 -0300

    Exercicio 2

--- fundamentos/bloco-8-higher-order-functions-js-es6/dia-3-js-es6-hof-map-e-filter/exercicios/Exercicio2.js
+++ fundamentos/bloco-8-higher-order-functions-js-es6/dia-3-js-es6-hof-map-e-filter/exercicios/Exercicio2.js
@@ -63,4 +63,42 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  let newArray = books.map((book) => (
+    {
+      age: book.releaseYear - book.author.birthYear,
+      author: book.author.name,
+    }
+  ));
+  newArray.sort((a,b) => a.age - b.age);
+  return newArray;
+}
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
