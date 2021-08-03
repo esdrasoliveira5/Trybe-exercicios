518c9a443c9239ac33269b4956ed30e4925aa4ab
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-08-03 19:30:22 -0300

    Exercicio 5

--- fundamentos/bloco-8-higher-order-functions-js-es6/dia-5-js-es6-spread-operator-rest-destructuring/exemplos/fixacaoparte2.js
+++ fundamentos/bloco-8-higher-order-functions-js-es6/dia-5-js-es6-spread-operator-rest-destructuring/exemplos/fixacaoparte2.js
@@ -18,6 +18,6 @@ console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 
-[comida] = [animal];
+[comida, animal, bebida ] = [bebida, comida, animal];
 
 console.log(comida, animal, bebida);
