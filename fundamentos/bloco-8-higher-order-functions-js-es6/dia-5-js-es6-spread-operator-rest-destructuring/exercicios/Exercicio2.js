f03df2da08e926bbcde0d88f01e963d7af055084
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-08-03 18:40:10 -0300

    Exercicio 2

---
+++ fundamentos/bloco-8-higher-order-functions-js-es6/dia-5-js-es6-spread-operator-rest-destructuring/exercicios/Exercicio2.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+function sum(...array) {
+  return array.reduce((total, number) => total + number, 0);
+}
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
