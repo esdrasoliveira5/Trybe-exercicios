ee765024f648963de0c995c4516d035486123659
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-08-17 19:47:01 -0300

    Parte 1 exercicio 1 e 2

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/exercicios/part1/src/Part1.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/exercicios/part1/src/Part1.js
@@ -7,6 +7,30 @@ function sum(a, b) {
   return a + b;
 }
 
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
 module.exports = {
   sum,
+  myRemove,
+  myRemoveWithoutCopy,
 }
--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/exercicios/part1/tests/Part1.spec.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/exercicios/part1/tests/Part1.spec.js
@@ -1,4 +1,4 @@
-const { sum } = require("../src/Part1")
+const { sum,  myRemove, myRemoveWithoutCopy } = require("../src/Part1");
 
 describe('Function Sum', () => {
   it('sum is a function', () => {
@@ -20,4 +20,55 @@ describe('Function Sum', () => {
   it('4 + "5" e igual a mensagem de erro', () => {
     expect(() => sum(4, '5')).toThrow('parameters must be numbers');
   })
+})
+
+describe('Function myRemove', () => {
+  it('myRemove', () => {
+    expect(typeof myRemove).toBe('function');
+  });
+
+  it('recebe o array [1, 2, 3, 4] e recebe [1, 2, 4]', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 3)).toStrictEqual([1, 2, 4]);
+  });
+
+  it('recebe o array [1, 2, 3, 4] e nao recebe [1, 2, 3, 4]', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 3)).not.toStrictEqual([1, 2, 3, 4]);
+  });
+
+  it('verifica se o array nao sofre alteracoes', () => {
+    const array = [1, 2, 3, 4];
+    myRemove(array, 3);
+    expect(array).toStrictEqual([1, 2, 3, 4]);
+  });
+
+  it('Verifica se o myRemove([1, 2, 3, 4], 5) retorna o esperado', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 5)).toStrictEqual([1, 2, 3, 4]);
+  });
+})
+
+describe('Function myRemoveWithoutCopy', () => {
+  it('Verifica se myRemoveWithoutCopy e uma funcao', () => {
+    expect(typeof myRemoveWithoutCopy).toBe('function');
+  });
+
+  it('Verifica se a myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toStrictEqual([1, 2, 4]);
+  });
+
+  it('Verifica se a myRemoveWithoutCopy([1, 2, 3, 4], 3) nao retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toStrictEqual(([1, 2, 3, 4]));
+  });
+
+  it('Verifica se o array passado por paramentro sofre alteracoes', () => {
+    const array = [1, 2, 3, 4];
+    myRemoveWithoutCopy(array, 3)
+    expect(array).not.toStrictEqual([1, 2, 3, 4]);
+  });
+
+  it('Verifica se a chamda myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toStrictEqual(([1, 2, 3, 4]));
+  });
 })
