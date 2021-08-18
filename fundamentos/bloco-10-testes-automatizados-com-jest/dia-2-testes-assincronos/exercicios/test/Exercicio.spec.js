437bc860d2247ae7e48118fd3bc913a157780714
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-08-18 20:57:42 -0300

    Exercicio 1

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-testes-assincronos/exercicios/test/Exercicio.spec.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-testes-assincronos/exercicios/test/Exercicio.spec.js
@@ -1,4 +1,6 @@
-const { uppercase } = require('../src/Exercicios');
+/* eslint-disable max-lines-per-function */
+/* eslint-disable max-len */
+const { uppercase, getUserName } = require('../src/Exercicios');
 
 describe('Exercise 1 test', () => {
   it('Function uppercase is defined', () => {
@@ -13,6 +15,26 @@ describe('Exercise 1 test', () => {
       } catch (error) {
         done(error);
       }
+    });
+  });
+});
+
+describe('Exercise 2 test', () => {
+  it('Function getUserName is defined', () => {
+    expect(typeof getUserName).toBe('function');
+  });
+
+  describe('if the id exist', () => {
+    it('return the user name', async () => {
+      expect.assertions(1);
+      await expect(getUserName(1)).resolves.toEqual('Mark');
+    });
+  });
+
+  describe('if the id not exist', () => {
+    it('return an error', async () => {
+      expect.assertions(1);
+      await expect(getUserName(4)).rejects.toEqual(new Error('User with 4 not found.'));
     });
   });
 });
