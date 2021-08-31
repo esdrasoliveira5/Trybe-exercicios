639ad9e52a257727e93640b8559cb84fbc3009b0
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-08-31 16:04:22 -0300

    Implementado componente Orders

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/fixacao/my-fixacao/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/fixacao/my-fixacao/src/App.js
@@ -1,12 +1,38 @@
-import Image from './Image';
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
+import Order from './Order';
 
-function App() {
-  return (
-    <main>
-      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+        < Order user={headphone.user} product={headphone.product} price={headphone.price} />
+        < Order user={energyDrink.user} product={energyDrink.product} price={energyDrink.price} />
+      </div>
+    );
+  }
 }
 
 export default App;
