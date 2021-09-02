6aedb4cd3e5e587783169b4db86ebc55586e03aa
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-09-02 18:22:26 -0300

    Fixacao 2

--- front-end/bloco-12-componentes-comestado-formularios-react/dia-1-componentes-com-estado-e-eventos/fixacao/fancy-buttons/src/App.js
+++ front-end/bloco-12-componentes-comestado-formularios-react/dia-1-componentes-com-estado-e-eventos/fixacao/fancy-buttons/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import React from 'react';
 
-function logPress() {
-  console.log('PRESS');
+function logPress1() {
+  console.log('PRESS11');
+}
+
+function logPress2() {
+  console.log('PRESS22');
+}
+function logPress3() {
+  console.log('PRESS33');
 }
 
 class App extends React.Component {
   render() {
     return (
-      <button onClick={logPress}>Button</button>
+      <div>
+        <button onClick={logPress1}>Button1</button>
+        <button onClick={logPress2}>Button2</button>
+        <button onClick={logPress3}>Button3</button>
+      </div>
     );
   }
 }
