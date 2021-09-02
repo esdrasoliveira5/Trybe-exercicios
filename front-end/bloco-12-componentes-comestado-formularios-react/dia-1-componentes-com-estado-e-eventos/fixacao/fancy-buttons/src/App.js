f2814ad5b8f376e6a8c95e76bb910af1ecc1a241
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-09-02 18:59:41 -0300

    Fixicacao 5,6,7

--- front-end/bloco-12-componentes-comestado-formularios-react/dia-1-componentes-com-estado-e-eventos/fixacao/fancy-buttons/src/App.js
+++ front-end/bloco-12-componentes-comestado-formularios-react/dia-1-componentes-com-estado-e-eventos/fixacao/fancy-buttons/src/App.js
@@ -7,18 +7,23 @@ class App extends React.Component {
   constructor() {
     super()
     this.logPress1 = this.logPress1.bind(this)
+    this.state = {
+      numeroDeCliques: 0
+    }
   }
 
-
-  logPress1() {
+  logPress1(string) {
     console.log('PRESS11');
-    console.log(this);
+    this.setState((s,_p) => ({
+      numeroDeCliques: s.numeroDeCliques + 1
+    }))
+    console.log(string + this.state.numeroDeCliques);
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.logPress1}>Button1</button>
+        <button onClick={()=> this.logPress1('LaMERDA')}>Botao Cliques: {this.state.numeroDeCliques}</button>
       </div>
     );
   }
