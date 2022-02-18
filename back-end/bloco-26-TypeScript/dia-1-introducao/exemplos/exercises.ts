c0d770e59aaafee179ef027e8c68de9d2cff7450
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-02-18 18:42:03 -0300

    Finalizando o dia

---
+++ back-end/bloco-26-TypeScript/dia-1-introducao/exemplos/exercises.ts
@@ -0,0 +1,33 @@
+// ./exercises.ts
+
+export function greeter(name: string) {
+  return `Olá ${name}!`;
+}
+
+export function personAge(name: string, age: number) {
+  return `${name} tem ${age} anos!`;
+}
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function sumArray(numbers: number[]): number {
+  return numbers.reduce(add, 0);
+}
+
+  export function triangle(base: number, height: number): number {
+    return (base * height) / 2;
+}
+
+export function square(side: number): number {
+  return side ** 2;
+}
+
+export function rectangle(base: number, height: number): number {
+  return base * height;
+}
+
+let idade:number = 10;
+
+idade = 40;
